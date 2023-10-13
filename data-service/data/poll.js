3fab2cd8151590a26282875154e6b3673c627313
Chia-Hsiang Wu <cwu34@stevens.edu>
2023-10-13 14:04:53 -0400

    data file

--- data-service/data/poll.js
+++ data-service/data/poll.js
@@ -4,18 +4,58 @@ import { ObjectId } from "mongodb";
 /**
  * @param {String} org_id
  * @param {String} event_id
- * @param {String} user_id
- * @param {String} poll_id
  * @param {String} title
  * @param {String} description
- * @param {String} option
  * @param {Array} options
  */
+const create = async (org_id, event_id, title, description, options) => {
+  const newPoll = {
+    poll_id: "",
+    org_id: "",
+    event_id: "",
+    title: "",
+    description: "",
+    options: {},
+  };
+};
 
-const create = async (org_id, event_id, title, description, options) => {};
+/**
+ *
+ * @param {String} poll_id
+ */
+const get = async (poll_id) => {};
 
+const getAll = async () => {};
+
+/**
+ *
+ * @param {String} event_id
+ */
+const getByEventId = async (event_id) => {};
+
+/**
+ *
+ * @param {String} user_id
+ * @param {String} poll_id
+ * @param {String} option
+ */
 const vote = async (user_id, poll_id, option) => {};
 
+/**
+ * @param {String} org_id
+ * @param {String} event_id
+ * @param {String} title
+ * @param {String} description
+ * @param {Array} options
+ */
 const update = async (org_id, event_id, title, description, options) => {};
 
+/**
+ *
+ * @param {String} user_id
+ * @param {String} poll_id
+ * @param {String} option
+ */
 const remove = async (user_id, poll_id) => {};
+
+export { create, get, getAll, getByEventId, vote, update, remove };
