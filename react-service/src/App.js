724d7da94a5bcef3734519c117fe9f99e86e7016
Yetong <cissieslab@gmail.com>
2023-10-14 20:32:09 -0400

    Add Calendar component to home page
    use react-calendar to construct the calendar component, adjust the style with Calendar.css

--- react-service/src/App.js
+++ react-service/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './App.css'
+import './Calendar.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      
+        <h1 className='text-center'>ReactCalendar</h1>
+        <div className='calendar-container' style={{display: 'flex', justifyContent:'center'}}>
+          <Calendar onChange={setDate} value={date}/>
+        </div>
+        <p className='text-center'>
+          <span className='bold'>SelectedDate:</span>{' '}{date.toDateString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
