62c55d97ea069ed4e36d422aa756c4742eeab17c
tzuminglu <1994515daniel@gmail.com>
2023-10-17 20:48:39 -0400

    XXX

--- data-service/test.js
+++ data-service/test.js
@@ -1,62 +1,65 @@
-import * as polls from "./data/poll.js";
+import * as pollData from "./data/poll.js";
 import { expect } from "chai";
+import { dbConnection, closeConnection } from "./config/mongoConnection.js";
 
 describe("poll function tests", async () => {
-  it("create poll", async () => {
-    before(async () => {
-      // runs before all tests in this file regardless where this line is defined.
-      const data = {
-        org_id: "1234567890",
-        event_id: "1234567890",
-        title: "new poll",
-        description: "this is a new poll",
-        options: ["opt1", "opt2", "opt3"],
-      };
+  it("Get all", async () => {
+    const db = await dbConnection();
+    await db.dropDatabase();
 
-      const poll = await polls.create(
-        data.org_id,
-        data.event_id,
-        data.title,
-        data.description,
-        data.options
-      );
-    });
+    const data1 = {
+      org_id: "1234567890",
+      event_id: "1234567890",
+      title: "new poll-1",
+      description: "this is a new poll-1",
+      options: ["opt1", "opt2", "opt3"],
+    };
 
-    const poll = await polls.getAll();
+    const data2 = {
+      org_id: "0987654321",
+      event_id: "0987654321",
+      title: "new poll-2",
+      description: "this is a new poll-2",
+      options: ["opt1", "opt2", "opt3"],
+    };
+
+    await pollData.create(
+      data1.org_id,
+      data1.event_id,
+      data1.title,
+      data1.description,
+      data1.options
+    );
+    await pollData.create(
+      data2.org_id,
+      data2.event_id,
+      data2.title,
+      data2.description,
+      data2.options
+    );
+
+    const poll = await pollData.getAll();
     const first_poll = poll[0];
 
     expect(first_poll.org_id).to.equal("1234567890");
     expect(first_poll.event_id).to.equal("1234567890");
-    expect(first_poll.title).to.equal("new poll");
-    expect(first_poll.description).to.equal("this is a new poll");
+    expect(first_poll.title).to.equal("new poll-1");
+    expect(first_poll.description).to.equal("this is a new poll-1");
     expect(Array.isArray(first_poll.options.opt1)).to.equal(true);
     expect(Array.isArray(first_poll.options.opt2)).to.equal(true);
     expect(Array.isArray(first_poll.options.opt3)).to.equal(true);
-  });
 
-  it("vote", async () => {
-    const data = {
-      org_id: "1234567890",
-      event_id: "1234567890",
-      title: "new poll",
-      description: "this is a new poll",
-      options: ["opt1", "opt2", "opt3"],
-    };
+    const second_poll = poll[1];
 
-    let poll = await polls.create(
-      data.org_id,
-      data.event_id,
-      data.title,
-      data.description,
-      data.options
-    );
+    expect(second_poll.org_id).to.equal("0987654321");
+    expect(second_poll.event_id).to.equal("0987654321");
+    expect(second_poll.title).to.equal("new poll-2");
+    expect(second_poll.description).to.equal("this is a new poll-2");
+    expect(Array.isArray(second_poll.options.opt1)).to.equal(true);
+    expect(Array.isArray(second_poll.options.opt2)).to.equal(true);
+    expect(Array.isArray(second_poll.options.opt3)).to.equal(true);
 
-    expect(poll.org_id).to.equal("1234567890");
-    expect(poll.event_id).to.equal("1234567890");
-    expect(poll.title).to.equal("new poll");
-    expect(poll.description).to.equal("this is a new poll");
-    expect(Array.isArray(poll.options.opt1)).to.equal(true);
-    expect(Array.isArray(poll.options.opt2)).to.equal(true);
-    expect(Array.isArray(poll.options.opt3)).to.equal(true);
+    await db.dropDatabase();
+    await closeConnection();
   });
 });
