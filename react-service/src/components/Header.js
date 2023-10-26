de5d4661a8a65d83a7ae8a203c94d27f8642d1e2
Leona <huangsylvia822@gmail.com>
2023-10-26 01:47:27 -0400

    Update login/logout logic, if user has logined, the login should be change to logout

--- react-service/src/components/Header.js
+++ react-service/src/components/Header.js
@@ -1,45 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import '../styles/Home.css';
-import profile from '../images/profile-1.jpg';
-import {NavLink} from "react-router-dom";
-import { AppBar, Toolbar, Typography, IconButton, Button} from '@mui/material';
+import { NavLink } from "react-router-dom";
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const Header = () => {
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, setUser);
+        return () => unsubscribe();
+    }, [auth]);
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+            console.log('User signed out');
+        }).catch((error) => {
+            console.error('Sign out error:', error);
+        });
+    };
+
     const theme = createTheme({
         palette: {
-          primary: {
-            main: '#f6f6f9',
-          },
-         
+            primary: {
+                main: '#f6f6f9',
+            },
         },
-      });
+    });
 
     return (
         <ThemeProvider theme={theme}>
-       
             <AppBar class="AppBar" position="static">
                 <Toolbar variant="dense">
-                <IconButton
-                    size="medium"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <NavLink to="/"><MenuIcon /></NavLink>
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Events
-                </Typography>
-                <Button color="inherit"><NavLink to="/login">Login</NavLink></Button>
+                    <IconButton
+                        size="medium"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <NavLink to="/"><MenuIcon /></NavLink>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Events
+                    </Typography>
+                    <Button color="inherit" onClick={user ? handleLogout : null}>
+                        <NavLink to={user ? "/" : "/login"}>{user ? "Logout" : "Login"}</NavLink>
+                    </Button>
                 </Toolbar>
             </AppBar>
-
-     
         </ThemeProvider>
     );
-}
+};
 
 /*     
  <AppBar position = "static" justify = "center"><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
