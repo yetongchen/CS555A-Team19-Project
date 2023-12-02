8e3993f3cd49886360b796a71b668f9de5342b11
Yetong <cissieslab@gmail.com>
2023-12-01 23:00:33 -0500

    Refactor event detail pages

--- data-service/seed.js
+++ data-service/seed.js
@@ -2,7 +2,6 @@ import { createUser } from './data/users.js';
 import { dbConnection, closeConnection } from './config/mongoConnection.js';
 
 const db = await dbConnection();
-await db.dropDatabase();
 // password: Test555
 await createUser(
     "testName",
