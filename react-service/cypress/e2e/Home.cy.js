20063b3dca68547a45baf9a81b85147360b16afe
tzuminglu <1994515daniel@gmail.com>
2023-12-02 19:40:04 -0500

    update home button click test

--- react-service/cypress/e2e/Home.cy.js
+++ react-service/cypress/e2e/Home.cy.js
@@ -10,6 +10,23 @@ context('Navigation', () => {
       // reload the page without using the cache
       cy.reload(true)
     })
+
+    it('cy.click() - click botton', () => {
+      // Pass options to the visit
+      cy.visit('localhost:3000', {
+        timeout: 4000, // increase total time for the visit to resolve
+        onBeforeLoad (contentWindow) {
+          // contentWindow is the remote page's window object
+          expect(typeof contentWindow === 'object').to.be.true
+        },
+        onLoad (contentWindow) {
+          // contentWindow is the remote page's window object
+          expect(typeof contentWindow === 'object').to.be.true
+        },
+      })
+
+      cy.get('.date-submit-btn').click();
+    })
   
     it('cy.visit() - visit the login page', () => {
       // Visit any sub-domain of your current domain
